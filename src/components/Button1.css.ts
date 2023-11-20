bfb191607292095b48dfc65155590e06693fb50f
Adam Skoufis <askoufis@seek.com.au>
2023-11-20 17:15:28 +1100

    Add extra styles to buttons 1, 2 and 3

--- src/components/Button1.css.ts
+++ src/components/Button1.css.ts
@@ -1,3 +1,6 @@
 import { sprinkles } from "../styles/sprinkles.css.ts";
+import { style } from "@vanilla-extract/css";
 
 export const button1 = sprinkles({ paddingTop: "80px" });
+
+export const anotherStyle = style({ color: "red" });
--- src/components/Button2.css.ts
+++ src/components/Button2.css.ts
@@ -1,3 +1,6 @@
 import { sprinkles } from "../styles/sprinkles.css.ts";
+import { style } from "@vanilla-extract/css";
 
 export const button2 = sprinkles({ paddingTop: "80px" });
+
+export const anotherStyle = style({ color: "red" });
